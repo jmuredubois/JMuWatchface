8fba6c214ee1d5f3db1fd8f123ab02bab8c81ba0
jmuredubois <jmuredubois@outlook.com>
2015-01-01 13:00:43 +0100

    added pebble battery state

--- src/jmuWatch.c
+++ src/jmuWatch.c
@@ -4,6 +4,13 @@
 #define KEY_CONDITIONS 1
 #define KEY_WEATHID 2
 #define KEY_CITY 3
+#define PERS_OWMCODE 0
+#define PERS_OWMTEMP 1
+#define PERS_OWMDESC 2
+#define PERS_OWMLOCA 3
+#define PERS_OWMVALD 4
+#define MAX_COND 32
+#define MAX_LOCA 64
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
@@ -13,18 +20,65 @@ static GFont s_month_font;
 static GFont s_weather_font;
 static GFont s_temp_font;
 static GFont s_city_font;
+static GFont s_battery_font;
 static TextLayer *s_day_layer;
 static TextLayer *s_date_layer;
 static TextLayer *s_month_layer;
 static TextLayer *s_weather_layer;
 static TextLayer *s_temp_layer;
 static TextLayer *s_city_layer;
+static TextLayer *s_battery_layer;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 static GBitmap *s_weather_bitmap;
 static BitmapLayer *s_weathbmp_layer;
 static GBitmap *s_bt_bitmap;
 static BitmapLayer *s_bt_layer;
+static GBitmap *s_ba_bitmap;
+static BitmapLayer *s_ba_layer;
+static int owmcode;
+static int owmtemp;
+static char owmdesc[MAX_COND];
+static char owmloca[MAX_LOCA];
+static int owmvald; // store time of weather update to invalidate old info
+
+static void battery_handler(BatteryChargeState charge_state) {
+  static char s_battery_buffer[16];
+
+  if (charge_state.is_charging) {
+    //snprintf(s_battery_buffer, sizeof(s_battery_buffer), "charging");
+    s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_CHARGE_WHITE);
+  } else {
+    if(charge_state.charge_percent < 25){
+      s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_ALMOSTEMPTY_WHITE);
+    } else {
+      if(charge_state.charge_percent < 50){
+        s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_25_WHITE);
+      } else {
+        if(charge_state.charge_percent < 75){
+          s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_50_WHITE);
+        } else {
+          if(charge_state.charge_percent < 85){
+            s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_75_WHITE);
+          } else {
+            if(charge_state.charge_percent < 95){
+              s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_ALMOSTFULL_WHITE);
+            } else {
+              s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_FULL_WHITE);
+            }
+          }  
+        }  
+      }
+    }
+    if(100==charge_state.charge_percent){
+      snprintf(s_battery_buffer, sizeof(s_battery_buffer), "1.");
+    } else{
+      snprintf(s_battery_buffer, sizeof(s_battery_buffer), ".%d", charge_state.charge_percent/10);
+    }
+  }
+  bitmap_layer_set_bitmap(s_ba_layer, s_ba_bitmap);
+  text_layer_set_text(s_battery_layer, s_battery_buffer);
+}
 
 void bt_handler(bool connected) {
   if (connected) {
@@ -84,8 +138,69 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     
     // Envoi du message
     app_message_outbox_send();
+    
+    // MaJ statut batterie
+    BatteryChargeState charge_state = battery_state_service_peek();
+    battery_handler(charge_state);
   }
 }
+
+static void set_owm_bmp(int owmcode) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Set_owm_bmp entered with code %d ...", owmcode);
+  s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_STORM_WHITE);
+  // cf. http://openweathermap.org/weather-conditions
+  if( (owmcode >=200) && (owmcode <300)){ // thunderstorm
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_STORM_WHITE);
+  }
+  if( owmcode ==211 ) { // thuderstorm
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CLOUD_LIGHTING_WHITE);
+  }
+  if( (owmcode >=300) && (owmcode <400)){ // drizzle
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LITTLE_RAIN_WHITE);
+  }
+  if( owmcode ==500 ) { // light rain
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LITTLE_RAIN_WHITE);
+  }
+  if( (owmcode >=500) && (owmcode <600)){ // rain
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_RAIN_WHITE);
+  }
+  if( owmcode ==600 ) { // light snow
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LITTLE_SNOW_WHITE);
+  }
+  if( (owmcode >600) && (owmcode <700)){ // snow
+    APP_LOG(APP_LOG_LEVEL_INFO, "WeathID: SNOW ");
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SNOW_WHITE);
+  }
+  if( (owmcode ==611) || (owmcode == 612) ) { // sleet
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SLEET_WHITE);
+  }
+  if( (owmcode >=700) && (owmcode <800)){ // atmosphere
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DUST_WHITE);
+  }
+  if( owmcode ==701 ) { // fog
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOG_DAY_WHITE);
+  }
+  if( owmcode ==741 ) { // fog
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOG_DAY_WHITE);
+  }
+  if( owmcode ==781 ) { // tornado
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO_WHITE);
+  }
+  if( owmcode ==800 ) { // clear sky
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SUN_WHITE);
+  }
+  if( (owmcode >800) && (owmcode <900)){ // clouds
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CLOUDS_WHITE);
+  }
+  if( owmcode ==900 ) { // tornade
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO_WHITE);
+  }
+  if( owmcode ==906 ){ // tornade
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_HAIL_WHITE);
+  }
+  APP_LOG(APP_LOG_LEVEL_INFO, "Set_owm_bmp returns ...");
+}
+
 
 static void main_window_load(Window *window) {
   // fonds noir
@@ -105,7 +220,6 @@ static void main_window_load(Window *window) {
   //police 
   //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   // Create GFont
-  //s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
   s_time_font = fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49);
 
   // Apply to TextLayer
@@ -141,45 +255,97 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_date_layer));
+
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached meteo validation in main window load ...");
+  
+  // invalidation meteo si trop vieille
+  if(persist_exists(PERS_OWMVALD)) {
+    // Get a tm structure
+    time_t current = time(NULL);
+    APP_LOG(APP_LOG_LEVEL_INFO, "Retrieving old meteo time ...");
+    owmvald = persist_read_int(PERS_OWMVALD);
+    if( ( (int)current - owmvald) > 10 ) {
+      // if weather is more than 5 min old
+      APP_LOG(APP_LOG_LEVEL_INFO, "Weather is too old, deleting.");
+      if(persist_exists(PERS_OWMDESC)) { persist_delete(PERS_OWMDESC); };
+      if(persist_exists(PERS_OWMCODE)) { persist_delete(PERS_OWMCODE); };
+      if(persist_exists(PERS_OWMTEMP)) { persist_delete(PERS_OWMTEMP); };
+      if(persist_exists(PERS_OWMLOCA)) { persist_delete(PERS_OWMLOCA); };
+      if(persist_exists(PERS_OWMVALD)) { persist_delete(PERS_OWMVALD); };
+    }  
+  }
   
   // ajout meteo
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached meteo display in main window load ...");
   s_weather_layer = text_layer_create(GRect(5, 138, 139, 24));
   text_layer_set_background_color(s_weather_layer, GColorClear);
   text_layer_set_text_color(s_weather_layer, GColorWhite);
   text_layer_set_text_alignment(s_weather_layer, GTextAlignmentLeft);
-  text_layer_set_text(s_weather_layer, "La meteo charge ...");
+  if(persist_exists(PERS_OWMDESC)) {
+    persist_read_string(PERS_OWMDESC, owmdesc, sizeof(owmdesc));
+    text_layer_set_text(s_weather_layer, owmdesc);
+  }
+  else {
+   text_layer_set_text(s_weather_layer, "La meteo charge ..."); 
+  }
   s_weather_font = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
   text_layer_set_font(s_weather_layer, s_weather_font);
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_weather_layer));
   
   // creation bitmap meteo
-  s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO);
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached meteo bitmap in main window load ...");
   s_weathbmp_layer = bitmap_layer_create(GRect(0, 88, 50, 50));
+  if(persist_exists(PERS_OWMCODE)) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Checking persisted owmcode");
+    owmcode = persist_read_int(PERS_OWMCODE);
+    APP_LOG(APP_LOG_LEVEL_INFO, "Calling set_owm_bmp in main window load ...");
+    set_owm_bmp(owmcode);
+  }
+  else {
+    s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO_BLACK);
+    layer_set_hidden(bitmap_layer_get_layer(s_weathbmp_layer),true);
+  }
   bitmap_layer_set_bitmap(s_weathbmp_layer, s_weather_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_weathbmp_layer));
-  layer_set_hidden(bitmap_layer_get_layer(s_weathbmp_layer),true);
 
   // ajout temperature
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached temperature display in main window load ...");
   s_temp_layer = text_layer_create(GRect(5, 88, 139, 34));
   text_layer_set_background_color(s_temp_layer, GColorClear);
   text_layer_set_text_color(s_temp_layer, GColorWhite);
   text_layer_set_text_alignment(s_temp_layer, GTextAlignmentRight);
-  text_layer_set_text(s_temp_layer, ".");
+  if(persist_exists(PERS_OWMTEMP)) {
+    owmtemp = persist_read_int(PERS_OWMTEMP);
+    static char temp_pers[8];
+    snprintf(temp_pers, sizeof(temp_pers), "%d°", owmtemp); // using this buffer as temp char buf
+    text_layer_set_text(s_temp_layer, temp_pers); 
+  }
+  else {
+   text_layer_set_text(s_temp_layer, "."); 
+  }
   s_temp_font = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
   text_layer_set_font(s_temp_layer, s_temp_font);
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_temp_layer));
   
   // ajout affichage ville et heure infos meteo
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached meteo location in main window load ...");
   s_city_layer = text_layer_create(GRect(0, 152, 144, 16));
   text_layer_set_background_color(s_city_layer, GColorClear);
   text_layer_set_text_color(s_city_layer, GColorBlack);
   text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter);
   s_city_font = fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD);
+  if(persist_exists(PERS_OWMLOCA)) {
+    persist_read_string(PERS_OWMLOCA, owmloca, sizeof(owmloca));
+    text_layer_set_text(s_city_layer, owmloca);
+  }
+  else {
+    layer_set_hidden(text_layer_get_layer(s_city_layer),true);
+  }
   text_layer_set_font(s_city_layer, s_city_font);
   layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_city_layer));
-  layer_set_hidden(text_layer_get_layer(s_city_layer),true);
   
   // abonnement aux evenements de connection bluetooth
+  APP_LOG(APP_LOG_LEVEL_INFO, "Reached BT subscription in main window load ...");
   bluetooth_connection_service_subscribe(bt_handler);
   
   // creation bitmap bluetooth
@@ -187,13 +353,47 @@ static void main_window_load(Window *window) {
   s_bt_layer = bitmap_layer_create(GRect(136, 0, 12, 12));
   bitmap_layer_set_bitmap(s_bt_layer, s_bt_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_layer));
+  
+  // abonnement aux evenements batterie
+  battery_state_service_subscribe(battery_handler);
+  // ajout affichage charge batterie
+  s_battery_layer = text_layer_create(GRect(124, -2, 14, 14));
+  text_layer_set_background_color(s_battery_layer, GColorClear);
+  text_layer_set_text_color(s_battery_layer, GColorWhite);
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentRight);
+  s_battery_font = fonts_get_system_font(FONT_KEY_GOTHIC_14);
+  text_layer_set_font(s_battery_layer, s_battery_font);
+  layer_add_child(window_get_root_layer(s_main_window), text_layer_get_layer(s_battery_layer));
+  
+  // creation bitmap batterie
+  s_ba_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BAT_CHARGE_WHITE);
+  s_ba_layer = bitmap_layer_create(GRect(114, 0, 12, 12));
+  bitmap_layer_set_bitmap(s_ba_layer, s_ba_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_ba_layer));
+
 
 }
 
 static void main_window_unload(Window *window) {
+  //destruction couche image batterie
+  gbitmap_destroy(s_ba_bitmap);
+  bitmap_layer_destroy(s_ba_layer);
+  // resiliation abonnement batterie ?
+  battery_state_service_unsubscribe();
+  // destruction couche batterie
+  text_layer_destroy(s_battery_layer);
+
   //destruction couche image meteo
   gbitmap_destroy(s_bt_bitmap);
   bitmap_layer_destroy(s_bt_layer);
+  // persistence de la meteo
+  APP_LOG(APP_LOG_LEVEL_INFO, "Saving weather data ...");
+  persist_write_int(PERS_OWMCODE, owmcode);
+  persist_write_int(PERS_OWMTEMP, owmtemp);
+  persist_write_string(PERS_OWMDESC, owmdesc);
+  persist_write_string(PERS_OWMLOCA, owmloca);
+  //persist_write_int(PERS_OWMVALD, owmvald);
+  APP_LOG(APP_LOG_LEVEL_INFO, "Weather data saved.");
   // resiliation abonnement bluetooth
   bluetooth_connection_service_unsubscribe();
   // destruction couche ville
@@ -225,10 +425,8 @@ static void main_window_unload(Window *window) {
 static void inbox_received_callback(DictionaryIterator *iterator, void *context){
   // Stockage pour meteo recue
   static char temperature_buffer[8];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[64];
-  static char city_buffer[64];
-  static char weathdt_buffer[16];
+  static char conditions_buffer[MAX_COND];
+  static char city_buffer[MAX_LOCA];
   
   // Lecture premier item
   Tuple *t = dict_read_first(iterator);
@@ -239,67 +437,22 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     switch(t->key) {
       case KEY_TEMPERATURE:
         snprintf(temperature_buffer, sizeof(temperature_buffer), "%d°", (int)t->value->int32);
+        owmtemp = (int)t->value->int32 ; // for persist write
         break;
       case KEY_WEATHID:  
-        s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO);
-        // cf. http://openweathermap.org/weather-conditions
-        if( ((int)t->value->int32 >=200) && ((int)t->value->int32 <300)){ // thunderstorm
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_STORM);
-        }
-        if( (int)t->value->int32 ==211 ) { // thuderstorm
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CLOUD_LIGHTING);
-        }
-        if( ((int)t->value->int32 >=300) && ((int)t->value->int32 <400)){ // drizzle
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LITTLE_RAIN);
-        }
-        if( (int)t->value->int32 ==500 ) { // light rain
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LITTLE_RAIN);
-        }
-        if( ((int)t->value->int32 >=500) && ((int)t->value->int32 <600)){ // rain
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_RAIN);
-        }
-        if( (int)t->value->int32 ==600 ) { // light snow
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LIGHT_SNOW);
-        }
-        if( ((int)t->value->int32 >600) && ((int)t->value->int32 <700)){ // snow
-            APP_LOG(APP_LOG_LEVEL_INFO, "WeathID: SNOW ");
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SNOW);
-        }
-        if( ((int)t->value->int32 ==611) || ((int)t->value->int32 == 612) ) { // sleet
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SLEET);
-        }
-        if( ((int)t->value->int32 >=700) && ((int)t->value->int32 <800)){ // atmosphere
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_DUST);
-        }
-        if( (int)t->value->int32 ==701 ) { // fog
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOG_DAY);
-        }
-        if( (int)t->value->int32 ==741 ) { // fog
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOG_DAY);
-        }
-        if( (int)t->value->int32 ==781 ) { // fog
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO);
-        }
-        if( (int)t->value->int32 ==800 ) { // clear sky
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_SUN);
-        }
-        if( ((int)t->value->int32 >800) && ((int)t->value->int32 <900)){ // clouds
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CLOUDS);
-        }
-        if( (int)t->value->int32 ==900 ) { // tornade
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO);
-        }
-        if( (int)t->value->int32 ==906 ){ // tornade
-            s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_HAIL);
-        }
+        s_weather_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TORNADO_BLACK);
+        owmcode = (int)t->value->int32 ; // for persist write
+        set_owm_bmp(owmcode);
         bitmap_layer_set_bitmap(s_weathbmp_layer, s_weather_bitmap);
         layer_set_hidden(bitmap_layer_get_layer(s_weathbmp_layer),false);
         break;
       case KEY_CONDITIONS:
         snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+        snprintf(owmdesc, sizeof(owmdesc), "%s", t->value->cstring); // for persist write
         break;
       case KEY_CITY:
         snprintf(city_buffer, sizeof(city_buffer), "%s", t->value->cstring);
+        snprintf(owmloca, sizeof(owmloca), "%s", t->value->cstring); // for persist write
         text_layer_set_text(s_city_layer, city_buffer);
         layer_set_hidden(text_layer_get_layer(s_city_layer),false);
         APP_LOG(APP_LOG_LEVEL_INFO, "Ville meteo : %s", t->value->cstring);
@@ -313,9 +466,11 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     t = dict_read_next(iterator);
   }
   // Assemblage chaine finale et affichage
-  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", conditions_buffer, temperature_buffer);
   text_layer_set_text(s_weather_layer, conditions_buffer);
   text_layer_set_text(s_temp_layer, temperature_buffer);
+  APP_LOG(APP_LOG_LEVEL_INFO, "Saving time for weather persistence ...");
+  //owmvald = (int) time(NULL);
+  APP_LOG(APP_LOG_LEVEL_INFO, "OWM validation time saved.");
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context){
@@ -354,6 +509,9 @@ static void init() {
   // affichage
   window_stack_push(s_main_window, true);
   update_time();
+  // MaJ statut batterie
+  BatteryChargeState charge_state = battery_state_service_peek();
+  battery_handler(charge_state);
 }
 
 static void deinit() {
